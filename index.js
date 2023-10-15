e3a61b0d454b1ab2422e3c97ce3881aa615f80ce
Hasibul Islam Nirob <49409997+hasibul-islam-nirob@users.noreply.github.com>
2023-10-15 13:40:31 +0600

    List Rendering 3

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
 var app = Vue.createApp({
     data() {
         return {
-            skills: [
-                { name: "HTML", experience: 5 },
-                { name: "CSS", experience: 10 },
-                { name: "JS", experience: 15 }
-            ],
-            newSkill: {}
+            skills: [],
+            newSkill: ''
         };
     },
 
     methods: {
         addSkill() {
             this.skills.push({ name: this.newSkill, experience: 0 });
-            this.newSkill = {};
+            this.newSkill = '';
         },
         removeSkill(i) {
             this.skills.splice(i, 1);
